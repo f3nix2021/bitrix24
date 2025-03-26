6f6edb70991d7c9f9b75ed3c89f9690023791daa
Roman Likhach <r.likhach@ya.ru>
2025-03-26 19:29:27 +0300

    folder12

---
+++ public-page/index.php
@@ -0,0 +1,20 @@
+<?php
+require $_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php";
+
+/**
+ * @var CMain $APPLICATION
+ */
+
+$APPLICATION->SetTitle("Список пользователей");
+?>
+<?
+$APPLICATION->IncludeComponent(
+	'habr_example:user.grid',
+	'',
+	[
+		'AJAX_MODE' => 'Y',
+		'AJAX_OPTION_JUMP' => 'N',
+		'AJAX_OPTION_HISTORY' => 'N',
+	],
+);
+?><?require $_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php";?>
