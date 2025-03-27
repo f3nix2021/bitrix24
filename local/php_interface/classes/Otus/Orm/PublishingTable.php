b55a93e893237bc16801fc9a65745d9d18433574
Roman Likhach <r.likhach@ya.ru>
2025-03-27 18:00:44 +0300

    new file and folder

---
+++ local/php_interface/classes/Otus/Orm/PublishingTable.php
@@ -0,0 +1,32 @@
+<?php
+namespace Otus\Orm;
+//sfsfsfsf
+use Bitrix\Main\ORM\Data\DataManager;
+use Bitrix\Main\ORM\Fields\FloatField;
+use Bitrix\Main\ORM\Fields\IntegerField;
+use Bitrix\Main\ORM\Fields\StringField;
+
+class PublishingTable extends DataManager
+{
+    public static function getTableName()
+    {
+        return 'o_publishing';
+    }
+
+    public static function getMap()
+    {
+        return [
+            (new IntegerField('ID'))
+                ->configurePrimary()
+                ->configureAutocomplete(),
+
+            (new StringField('TITLE'))
+                ->configureRequired(),
+
+            (new StringField('CITY')),
+
+            (new FloatField('AUTHOR_PROFIT'))
+                ->configureRequired(),
+        ];
+    }
+}
